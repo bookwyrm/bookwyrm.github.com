f3104b15e81986e9eabfb7c09d64916fc763bfae
Matt Vanderpol <matt@vanderpol.net>
2012-02-05 06:58:48 -0800

    Alternate gist_tag plugin which will hopefully work on github pages

--- _plugins/gist_tag.rb
+++ _plugins/gist_tag.rb
@@ -1,3 +1,10 @@
+# A Liquid tag for Jekyll sites that allows embedding Gists and showing code for non-JavaScript enabled browsers and readers.
+# by: Brandon Tilly
+# Source URL: https://gist.github.com/1027674
+# Post http://brandontilley.com/2011/01/31/gist-tag-for-jekyll.html
+#
+# Example usage: {% gist 1027674 gist_tag.rb %} //embeds a gist for this plugin
+
 require 'cgi'
 require 'digest/md5'
 require 'net/https'
@@ -9,7 +16,7 @@ module Jekyll
       super
       @text           = text
       @cache_disabled = false
-      @cache_folder   = File.expand_path "../_gist_cache", File.dirname(__FILE__)
+      @cache_folder   = File.expand_path "../.gist-cache", File.dirname(__FILE__)
       FileUtils.mkdir_p @cache_folder
     end
 
@@ -26,7 +33,10 @@ module Jekyll
 
     def html_output_for(script_url, code)
       code = CGI.escapeHTML code
-      %Q!<script src="#{script_url}"></script><div><noscript><pre><code>#{code}</code></pre></noscript></div>!
+      <<-HTML
+<div><script src='#{script_url}'></script>
+<noscript><pre><code>#{code}</code></pre></noscript></div>
+      HTML
     end
 
     def script_url_for(gist_id, filename)
@@ -61,7 +71,13 @@ module Jekyll
     def get_gist_from_web(gist, file)
       gist_url          = get_gist_url_for gist, file
       raw_uri           = URI.parse gist_url
-      https             = Net::HTTP.new raw_uri.host, raw_uri.port
+      proxy             = ENV['http_proxy']
+      if proxy
+        proxy_uri       = URI.parse(proxy)
+        https           = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new raw_uri.host, raw_uri.port
+      else
+        https           = Net::HTTP.new raw_uri.host, raw_uri.port
+      end
       https.use_ssl     = true
       https.verify_mode = OpenSSL::SSL::VERIFY_NONE
       request           = Net::HTTP::Get.new raw_uri.request_uri
